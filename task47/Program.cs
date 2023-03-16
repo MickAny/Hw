8bc723907a42d944d96f4714cb55a3f49ec3b294
Mikhail <aniskevich_mikhail@mail.ru>
2023-03-16 12:27:20 +0300

    Task_47

---
+++ task47/Program.cs
@@ -0,0 +1,28 @@
+﻿int Promt(string message)
+{
+  Console.WriteLine(message);
+  int num = Convert.ToInt32(Console.ReadLine());
+  return num;
+}
+
+double[,] CreateOutputArray(int m, int n)
+{
+  Random x = new Random();
+  double[,] arr = new double[m, n];
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i, j] = Convert.ToDouble(x.Next(-10, 10)/10.0);
+      Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+  Console.WriteLine();
+  return arr;
+}
+
+int m = Promt ("Введите количество строк: ");
+int n = Promt ("Введите количество столбцов: ");
+
+Console.WriteLine(CreateOutputArray(m, n));
