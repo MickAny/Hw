98e3d21c7bafbb3162cc8ff3450af8110f0e9ae1
Mikhail <aniskevich_mikhail@mail.ru>
2023-03-25 00:45:43 +0300

    Task_68

---
+++ task68/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(a>0 && b>0?"Вычисление функции Аккермана: "+ Function(a, b):"Числа должны быть положительными!");
+    
+
+int Function(int a, int b)
+{
+if (a==0)   return b+1;
+else if ((a!=0)&&(b==0))  return Function(a-1,1);
+else return Function(a-1, Function(a,b-1));
+}
+
+/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
